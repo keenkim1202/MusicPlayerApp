c242607ec7d8fd79f028a58df2e310b50402cc27
KEEN <homurahomu@naver.com>
2021-04-23 14:19:24 +0900

    프로젝트 생성

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by KEEN on 2021/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+
+
+}
+
