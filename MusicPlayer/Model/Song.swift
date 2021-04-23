44d687eb1cee7bb91c0ae480c35a10f04fc5d07b
KEEN <homurahomu@naver.com>
2021-04-23 14:50:28 +0900

    모델 생성 및 음원파일 추가

---
+++ MusicPlayer/Model/Song.swift
@@ -0,0 +1,16 @@
+//
+//  Song.swift
+//  MusicPlayer
+//
+//  Created by KEEN on 2021/04/23.
+//
+
+import Foundation
+
+struct Song {
+  var name: String
+  var image: String
+  var artist: String
+  var favorite: Bool
+  var fileName: String
+}
